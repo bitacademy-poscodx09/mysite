9f07b5631fe14e42a59a93a3d0b8ade6b6ee529e
bitacademy-poscodx09 <bitacademy.poscodx@gmail.com>
2024-12-30 14:10:53 +0900

    AuthInterceptor #3

--- mysite03/src/main/java/mysite/security/LoginInterceptor.java
+++ mysite03/src/main/java/mysite/security/LoginInterceptor.java
@@ -37,6 +37,8 @@ public class LoginInterceptor implements HandlerInterceptor {
 		
 		response.sendRedirect(request.getContextPath());
 		
+		System.out.println("[authUser]" + authUser);
+		
 		return false;
 	}
 }
